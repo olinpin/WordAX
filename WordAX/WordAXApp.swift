3d36a087714c39ae96dbc7440eb35c3674cd5f63
oliverhnat <oliver.hnat@gmail.com>
2024-02-19 10:17:28 +0100

    Initial Commit

---
+++ WordAX/WordAXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordAXApp.swift
+//  WordAX
+//
+//  Created by Oliver Hnát on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordAXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
