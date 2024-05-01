6b0644bbefd03cd6670bc3ab0da3ba493b27253e
oliverhnat <oliver.hnat@gmail.com>
2024-05-01 13:23:08 +0200

    Add Data model for decks

---
+++ WordAX/Model/Deck/Deck+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Deck+CoreDataClass.swift
+//  WordAX
+//
+//  Created by Oliver Hnát on 01.05.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Deck)
+public class Deck: NSManagedObject {
+
+}
---
+++ WordAX/Model/Deck/Deck+CoreDataProperties.swift
@@ -0,0 +1,44 @@
+//
+//  Deck+CoreDataProperties.swift
+//  WordAX
+//
+//  Created by Oliver Hnát on 01.05.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Deck {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Deck> {
+        return NSFetchRequest<Deck>(entityName: "Deck")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var flashcards: NSSet?
+
+}
+
+// MARK: Generated accessors for flashcards
+extension Deck {
+
+    @objc(addFlashcardsObject:)
+    @NSManaged public func addToFlashcards(_ value: Flashcard)
+
+    @objc(removeFlashcardsObject:)
+    @NSManaged public func removeFromFlashcards(_ value: Flashcard)
+
+    @objc(addFlashcards:)
+    @NSManaged public func addToFlashcards(_ values: NSSet)
+
+    @objc(removeFlashcards:)
+    @NSManaged public func removeFromFlashcards(_ values: NSSet)
+
+}
+
+extension Deck : Identifiable {
+
+}
